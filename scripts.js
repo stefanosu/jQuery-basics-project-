11fd388ceff70e90d65f5ea427631984cb5bcc40
Stefanos <stefanosu422@gmail.com>
2016-04-30 15:02:14 -0400

    Added files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// This is the keydown function that enables movement of the jpg img.
+$(document).ready(function() {
+	$(document).keydown(function(key) {
+		switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				$('img').animate({top:"-=10px"}, 'fast');
+				// Put our code here
+				break;
+			// Right Arrow Pressed
+			case 39:
+				$('img').animate({left:"+=10px"}, 'fast');
+				// Put our code here
+				break;
+			// Down Arrow Pressed
+			case 40:
+				$('img').animate({top:"+=10px"}, 'fast');
+				// Put our code here
+				break;
+		}
+	});
+});
